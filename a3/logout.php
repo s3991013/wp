5e563da459cf581d9ea7342ee7027b9731f981e5
s3991013 <s3991013@student.rmit.edu.au>
2024-11-08 18:05:57 +1100

    Update logout.php

--- a3/logout.php
+++ a3/logout.php
@@ -18,10 +18,10 @@ $successMessage = 'You have been logged out successfully.';
     <link href="https://fonts.googleapis.com/css2?family=Ysabeau+SC&family=Poetsen+One&display=swap&family=Permanent+Marker&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="styles.css">
-    <?php include 'includes\header.inc'; ?>
+    <?php include 'header.inc'; ?>
 </head>
 <body>
-<?php include 'includes\nav.inc'; ?>
+<?php include 'nav.inc'; ?>
 
 <div class="container mt-5">
     <h2 class="text-center mb-4">Logout</h2>
@@ -38,6 +38,6 @@ $successMessage = 'You have been logged out successfully.';
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<?php include 'includes\footer.inc'; ?>
+<?php include 'footer.inc'; ?>
 </body>
 </html>
